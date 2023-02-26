4af01037ff61cc1643609f027dbc457c688600ea
mammadnm <mammadnm@CATR4>
2023-02-26 13:22:14 +0400

    class

---
+++ ConsoleApp5/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Book
+    {
+        public string name;
+        public string authorName;
+        public string genre;
+        public int pageCount;
+        public double price;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{name} , {authorName} , {genre} , {pageCount}  ,  {price}");
+        }
+
+
+
+    }
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,55 +1,69 @@
 ﻿using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp5
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            label1:
-            Console.Write("ededi daxil edin: ");
-            string stl= Console.ReadLine();
-            bool situate = int.TryParse(stl, out int eded);
-            int say=(int)Math.Log10(eded)+1;
-            int eded2=0;
-            if (!situate)
-            {
-                goto label1;
-            }
-            while (eded > 0)
-            {
-                if (say % 2 == 0)
-                {
-                    int qaliq = eded % 10;
-                    eded = eded / 100;
-                    eded2 = eded2  * 10 + qaliq;
-                        
-                }
-                else
-                {
-                    eded = eded / 10;
-                    int qaliq = eded % 10;
-                    eded = eded / 10;
-                    eded2 = eded2 * 10 + qaliq;
+        { 
 
-                    
 
-                }
+            #region 11
+            //label1:
+            //Console.Write("ededi daxil edin: ");
+            //string stl= Console.ReadLine();
+            //bool situate = int.TryParse(stl, out int eded);
+            //int say=(int)Math.Log10(eded)+1;
+            //int eded2=0;
+            //if (!situate)
+            //{
+            //    goto label1;
+            //}
+            //while (eded > 0)
+            //{
+            //    if (say % 2 == 0)
+            //    {
+            //        int qaliq = eded % 10;
+            //        eded = eded / 100;
+            //        eded2 = eded2  * 10 + qaliq;
 
-            }
-            int sum = 0;
-            while (eded2 > 0)
-            {
-                int qaliq2 = eded2 % 10;
-                eded2 = eded2 / 10;
-                sum = sum * 10 + qaliq2;
-
-            }
-            Console.WriteLine(sum);
-           
+            //    }
+            //    else
+            //    {
+            //        eded = eded / 10;
+            //        int qaliq = eded % 10;
+            //        eded = eded / 10;
+            //        eded2 = eded2 * 10 + qaliq;
 
 
 
+            //    }
+
+            //}
+            //int sum = 0;
+            //while (eded2 > 0)
+            //{
+            //    int qaliq2 = eded2 % 10;
+            //    eded2 = eded2 / 10;
+            //    sum = sum * 10 + qaliq2;
+
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            Book info=new Book();
+            info.name = "King Arthur";
+            info.genre = "action ,";
+            info.authorName = "scarlett ,";
+            info.pageCount = 342; 
+            info.price = 29.99;  
+          
+            info.PrintInfo();
+
+
+
+          
 
 
         }
